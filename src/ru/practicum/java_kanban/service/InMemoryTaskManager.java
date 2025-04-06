6298e14aee82857caff93ee52b4e4f561f33bf54
TatyanaEn <enatskayatatyana@yandex.ru>
2025-04-06 23:16:51 +0300

    Спринт8.Исправление.

--- src/ru/practicum/java_kanban/service/InMemoryTaskManager.java
+++ src/ru/practicum/java_kanban/service/InMemoryTaskManager.java
@@ -20,12 +20,7 @@ public class InMemoryTaskManager implements TaskManager {
 
     private HistoryManager historyManager;
 
-    private final Set<Task> prioritizedTasks = new TreeSet<>(new Comparator<Task>() {
-        @Override
-        public int compare(Task t1, Task t2) {
-            return t1.getStartTime().compareTo(t2.getStartTime());
-        }
-    });
+    private final Set<Task> prioritizedTasks = new TreeSet<>((t1, t2) -> t1.getStartTime().compareTo(t2.getStartTime()));
 
     public InMemoryTaskManager() {
         this.tasks = new HashMap<>();
